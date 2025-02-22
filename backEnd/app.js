0041a566e715090f18fedef6400f76f163f09d8a
Karan <karansbisht7@gmail.com>
2025-02-22 13:08:10 +0530

    Added CRON

--- backEnd/app.js
+++ backEnd/app.js
@@ -4,8 +4,14 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const cron = require("node-cron");
 const db = require("./config/db");
 db();
+
+cron.schedule("*/5 * * * *", () => {
+  exec("curl https://fit-vibez.onrender.com");
+  console.log("Corn running....");
+});
 
 app.use(express.json());
 app.use(cors("*"));
